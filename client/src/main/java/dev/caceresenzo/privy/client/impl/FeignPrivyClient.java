ddb2c1190fe8034b0c082ab5a99225f2a9e12065
Caceresenzo <caceresenzo1502@gmail.com>
2026-09-19 21:38:03 +0200

    feat(feign): remove `/api/` from the path

--- client/src/main/java/dev/caceresenzo/privy/client/impl/FeignPrivyClient.java
+++ client/src/main/java/dev/caceresenzo/privy/client/impl/FeignPrivyClient.java
@@ -16,56 +16,56 @@ public interface FeignPrivyClient {
 
 	String JSON_CONTENT_TYPE = "Content-Type: application/json";
 
-	@RequestLine("GET /api/v1/users?limit={limit}&cursor={cursor}")
+	@RequestLine("GET /v1/users?limit={limit}&cursor={cursor}")
 	Page<User> getUsers(@Param long limit, @Param String cursor);
 
-	@RequestLine("POST /api/v1/users/search")
+	@RequestLine("POST /v1/users/search")
 	@Headers(JSON_CONTENT_TYPE)
 	Page<User> searchUsers(SearchRequest body);
 
-	@RequestLine("GET /api/v1/users/{id}")
+	@RequestLine("GET /v1/users/{id}")
 	User getUserById(@Param String id);
 
-	@RequestLine("DELETE /api/v1/users/{id}")
+	@RequestLine("DELETE /v1/users/{id}")
 	void deleteUserById(@Param String id);
 
-	@RequestLine("POST /api/v1/users/email/address")
+	@RequestLine("POST /v1/users/email/address")
 	@Headers(JSON_CONTENT_TYPE)
 	User getUserByEmail(AddressRequest body);
 
-	@RequestLine("POST /api/v1/users/wallet/address")
+	@RequestLine("POST /v1/users/wallet/address")
 	@Headers(JSON_CONTENT_TYPE)
 	User getUserByWallet(AddressRequest body);
 
-	@RequestLine("POST /api/v1/users/phone/number")
+	@RequestLine("POST /v1/users/phone/number")
 	@Headers(JSON_CONTENT_TYPE)
 	User getUserByPhone(PhoneRequest body);
 
-	@RequestLine("POST /api/v1/users/twitter/username")
+	@RequestLine("POST /v1/users/twitter/username")
 	@Headers(JSON_CONTENT_TYPE)
 	User getUserByTwitterUsername(UsernameRequest body);
 
-	@RequestLine("POST /api/v1/users/twitter/subject")
+	@RequestLine("POST /v1/users/twitter/subject")
 	@Headers(JSON_CONTENT_TYPE)
 	User getUserByTwitterSubject(SubjectRequest body);
 
-	@RequestLine("POST /api/v1/users/discord/username")
+	@RequestLine("POST /v1/users/discord/username")
 	@Headers(JSON_CONTENT_TYPE)
 	User getUserByDiscordUsername(UsernameRequest body);
 
-	@RequestLine("POST /api/v1/users/github/username")
+	@RequestLine("POST /v1/users/github/username")
 	@Headers(JSON_CONTENT_TYPE)
 	User getUserByGithubUsername(UsernameRequest body);
 
-	@RequestLine("POST /api/v1/users/custom_auth/id")
+	@RequestLine("POST /v1/users/custom_auth/id")
 	@Headers(JSON_CONTENT_TYPE)
 	User getUserByCustomAuthId(CustomUserIdRequest body);
 
-	@RequestLine("POST /api/v1/users/{id}/custom_metadata")
+	@RequestLine("POST /v1/users/{id}/custom_metadata")
 	@Headers(JSON_CONTENT_TYPE)
 	User setCustomMetadata(@Param String id, CustomMetadataUpdateRequest body);
 
-	@RequestLine("GET /api/v1/apps/{applicationId}")
+	@RequestLine("GET /v1/apps/{applicationId}")
 	ApplicationSettings getApplicationSettings(@Param String applicationId);
 
 	static record SearchRequest(
